122c8468736bfd211ced8421a6c6dc43f5409342
Viktoryia Butrym <Viktoryia_Butrym@epam.com>
2023-05-22 20:50:52 +0300

    module-8: deploy to aws lambda

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 import { CartModule } from './cart/cart.module';
-import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-    AuthModule,
     CartModule,
     OrderModule,
   ],
