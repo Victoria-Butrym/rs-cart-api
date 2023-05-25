02da829c7e874bc020ddf44883a6431c99082f87
Viktoryia Butrym <Viktoryia_Butrym@epam.com>
2023-05-25 21:05:08 +0300

    removed unused code

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-
-// // import * as helmet from 'helmet';
-
-// import { AppModule } from './app.module';
-
-// const port = process.env.PORT || 4000;
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors({
-//     origin: (req, callback) => callback(null, true),
-//   });
-//   // app.use(helmet());
-
-//   await app.listen(port);
-// }
-// bootstrap().then(() => {
-//   console.log('App is running on %s port', port);
-// });
-
 import { NestFactory } from '@nestjs/core';
 import { Callback, Context, Handler } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
